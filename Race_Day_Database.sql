2d8a2ea165a6c3c9bee2036419fbdd59d24d602d
BaloyiMpho <ST10485231@rcconnect.edu.za>
2026-08-28 15:23:44 +0200

    Create Role Table in Race_Day Database
    Added Role Table to the Race_Day database schema.

--- Race_Day_Database.sql
+++ Race_Day_Database.sql
@@ -4,7 +4,7 @@ CREATE DATABASE Race_Day;
 
 Use Race_Day;
 
---1. User Tabkle
+--1. User Table
 CREATE TABLE [USER] (
     user_id INT IDENTITY(1,1) PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
@@ -17,5 +17,13 @@ CREATE TABLE [USER] (
     created_at DATETIME DEFAULT GETDATE(),
     last_login DATETIME,
     is_active BIT DEFAULT 1
+);
+GO
+--2     Role Table
+CREATE TABLE ROLE (
+    role_id INT IDENTITY(1,1) PRIMARY KEY,
+    role_name VARCHAR(30) UNIQUE NOT NULL,  
+    description VARCHAR(200),
+    created_at DATETIME DEFAULT GETDATE()
 );
 GO
